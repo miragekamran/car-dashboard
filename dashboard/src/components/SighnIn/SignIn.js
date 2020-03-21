499ca7e2d5e2e76a06edd9bbbe9f58c85f0f32e7
mirage <mirage.kamran.pkt@gmail.com>
2020-03-20 22:53:42 -0700

    project updated

--- dashboard/src/components/SighnIn/SignIn.js
+++ dashboard/src/components/SighnIn/SignIn.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
 import './SignIn.scss';
-import { useHistory } from 'react-router-dom';
+import {useHistory, Redirect} from 'react-router-dom';
 
-function App() {
+export default function SignIn(props) {
   const { register, handleSubmit, errors } = useForm()
   const [signInError, setSignInError] = useState(false)
   const history = useHistory();
-
-
+  
   const onSubmit = data => {
-      if (data.password === process.env.REACT_APP_PASSWORD) {
-          localStorage.setItem('token', '12345678')
+      if (data.password === process.env.REACT_APP_PASSWORD){
+          localStorage.setItem('token','12345678')
           setSignInError(false)
           history.push('/dashboard')
       } else {
-            setSignInError(true)
+          setSignInError(true)
+
       }
   }
    
+  if(localStorage.getItem("token")){
+     return <Redirect to="/dashboard/"/>
+  }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="username" name='username' ref={register({ required: true,  pattern: /^[a-z0-9_-]*$/igm })} />
-      {errors.username && 'Username Is Required'}
-      <input placeholder="password" type='password' name='password' ref={register({ pattern: /^(?=.*\d).{4,8}$/ })} />
-      {errors.password && 'Must contain a letter and number and be between 4 and 8 characters'}
+      <input placeholder="username" name="username" ref={ register({ required:true, pattern: /^[a-z0-9_-]*$/i})}/>
+      {errors.username && "Username Is Required"}
+      <input placeholder="password" type="password" name="password" ref={ register({ required:true,  pattern: /^(?=.*\d).{4,8}$/ })} />
+      {errors.password && "Password must contain a letter and number and be between 4 and 8 Characters"}
       <input type="submit" />
-      {signInError && "Username or Password Don't match"}
+      {signInError && "Username Or Password Don't Match"}
     </form>
   );
 }
-
-export default App;
